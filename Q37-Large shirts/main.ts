8cae4d447a19ae9724e657d79f6403e5b8371982
Maryam Tamkinat <160237233+maryamtamkinat@users.noreply.github.com>
2024-10-28 17:26:14 +0500

    Add files via upload

---
+++ Q37-Large shirts/main.ts
@@ -0,0 +1,15 @@
+//Q37-Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+
+function make_shirt (size : string = "Medium", printMessage : string = "I Love TypeScript"){
+    console.log(`You selected ${size} size with ${printMessage} prints on shirt`)
+}
+
+make_shirt();
+  
+//Call a function with large siza and default msg
+
+make_shirt("Large");
+
+//Call a function with samall siza but diffrent msg
+
+make_shirt("Small", "JK");
