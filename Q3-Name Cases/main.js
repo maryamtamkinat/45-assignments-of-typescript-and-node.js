cdbf7fa036941cdabe3a83d840f35be18102264f
Maryam Tamkinat <160237233+maryamtamkinat@users.noreply.github.com>
2024-10-28 16:44:26 +0500

    Add files via upload

---
+++ Q3-Name Cases/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Q3-Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let person_name = "Maryam tamkinat";
+console.log(person_name.toUpperCase());
+console.log(person_name.toLowerCase());
+console.log(person_name.replace(/\b\w/g, (char) => char.toUpperCase()));
