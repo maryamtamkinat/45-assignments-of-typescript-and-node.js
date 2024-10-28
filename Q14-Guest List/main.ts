4bde4de4296d2c984f29497da8b832d21b0c7738
Maryam Tamkinat <160237233+maryamtamkinat@users.noreply.github.com>
2024-10-28 16:52:28 +0500

    Add files via upload

---
+++ Q14-Guest List/main.ts
@@ -0,0 +1,4 @@
+//Q14-Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let guestList = ["Hania", "Ibad", "Mahnoor", "Ammar"];
+guestList.forEach(guest => console.log(`Assalam o alaikum! ${guest},Today would to like dinner with me?`));
