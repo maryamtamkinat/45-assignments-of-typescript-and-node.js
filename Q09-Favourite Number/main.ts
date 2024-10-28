50cad260bcbffbe13cb5d634af9884691c4a1b6c
Maryam Tamkinat <160237233+maryamtamkinat@users.noreply.github.com>
2024-10-28 17:24:34 +0500

    Add files via upload

---
+++ Q09-Favourite Number/main.ts
@@ -0,0 +1,3 @@
+//Q-9 Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let fav_num : number = 3;
+console.log(fav_num,"is my favourite number.");
