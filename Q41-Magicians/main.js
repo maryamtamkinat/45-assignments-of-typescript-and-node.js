0f432e20834166fb1cd185c586ed11a40f2c67b1
Maryam Tamkinat <160237233+maryamtamkinat@users.noreply.github.com>
2024-10-28 17:28:16 +0500

    Add files via upload

---
+++ Q41-Magicians/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+let magicians_name = ["Alice", "David", "Chris"];
+magicians_name.forEach(function show_magicians(magician) {
+    console.log(magician);
+});
