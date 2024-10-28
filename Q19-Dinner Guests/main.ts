31da760322697f4b5b3b110d1533ce4519f07283
Maryam Tamkinat <160237233+maryamtamkinat@users.noreply.github.com>
2024-10-28 17:02:58 +0500

    Add files via upload

---
+++ Q19-Dinner Guests/main.ts
@@ -0,0 +1,5 @@
+//Q19- Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let guestList = ["Hania", "Ibad", "Mahnoor", "Ammar"];
+
+console.log(`We are inviting total ${guestList.length} guests`);
