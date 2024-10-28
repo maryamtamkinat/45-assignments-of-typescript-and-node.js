053af6adb3777c70cd1a071bfc7f5539e069a371
Maryam Tamkinat <160237233+maryamtamkinat@users.noreply.github.com>
2024-10-28 17:04:12 +0500

    Add files via upload

---
+++ Q21-Creating Object/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Q21-They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let student = {
+    fullName: "Maryam Tamkinat",
+    age: 16,
+    marks: 80,
+    isPass: true,
+};
+console.log(student);
---
+++ Q21-Creating Object/main.ts
@@ -0,0 +1,18 @@
+//Q21-They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+interface student {
+    fullName : string;
+    age : number;
+    marks : number;
+    isPass : boolean;
+}
+
+let student = {
+    fullName : "Maryam Tamkinat",
+    age : 16,
+    marks : 80,
+    isPass : true,
+};
+
+console.log(student);
+    
