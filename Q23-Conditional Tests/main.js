e82747ff59e5589fa36340f1c33ae5a2c647e6ca
Maryam Tamkinat <160237233+maryamtamkinat@users.noreply.github.com>
2024-10-28 17:05:54 +0500

    Add files via upload

---
+++ Q23-Conditional Tests/main.js
@@ -0,0 +1,34 @@
+"use strict";
+//Q23-Conditional Tests: Write a series of conditional tests
+let value1 = 20;
+let value2 = 40;
+//Test 1
+console.log("Test 1: 20 is equal to 20?");
+console.log(value1 == 20);
+//Test 2
+console.log("Test 2: 40 is equal to 40?");
+console.log(value2 == 40);
+//Test 3
+console.log("Test 3: 20 is not equal to 40?");
+console.log(value1 != value2);
+//Test 4
+console.log("Test 4: 40 is greater than 20?");
+console.log(value2 > value1);
+//Test 5
+console.log("Test 5: 20 is less than 40?");
+console.log(value1 < value2);
+//Test 6
+console.log("Test 6: 20 is equal to 40?");
+console.log(value1 == 40);
+//Test 7
+console.log("Test 7: 40 is equal to 20?");
+console.log(value2 == 20);
+//Test 8
+console.log("Test 8: 20 is not equal to 20?");
+console.log(value1 != value1);
+//Test 9
+console.log("Test 9: 40 is less than 20?");
+console.log(value2 < value1);
+//Test 10
+console.log("Test 10: 20 is greater than 40?");
+console.log(value1 > value2);
---
+++ Q23-Conditional Tests/main.ts
@@ -0,0 +1,34 @@
+//Q23-Conditional Tests: Write a series of conditional tests
+
+let value1 = 20;
+let value2 = 40;
+//Test 1
+console.log("Test 1: 20 is equal to 20?");
+console.log(value1 == 20);
+//Test 2
+console.log("\nTest 2: 40 is equal to 40?");
+console.log(value2 == 40);
+//Test 3
+console.log("\nTest 3: 20 is not equal to 40?");
+console.log(value1 != value2);
+//Test 4
+console.log("\nTest 4: 40 is greater than 20?");
+console.log(value2 > value1);
+//Test 5
+console.log("\nTest 5: 20 is less than 40?");
+console.log(value1 < value2);
+//Test 6
+console.log("\nTest 6: 20 is equal to 40?");
+console.log(value1 == 40);
+//Test 7
+console.log("\nTest 7: 40 is equal to 20?");
+console.log(value2 == 20);
+//Test 8
+console.log("\nTest 8: 20 is not equal to 20?");
+console.log(value1 != value1);
+//Test 9
+console.log("\nTest 9: 40 is less than 20?");
+console.log(value2 < value1);
+//Test 10
+console.log("\nTest 10: 20 is greater than 40?");
+console.log(value1 > value2);
