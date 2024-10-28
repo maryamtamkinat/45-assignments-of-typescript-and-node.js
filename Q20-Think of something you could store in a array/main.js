289245fea1a831b33f7908863e3b19438566ad1b
Maryam Tamkinat <160237233+maryamtamkinat@users.noreply.github.com>
2024-10-28 17:03:32 +0500

    Add files via upload

---
+++ Q20-Think of something you could store in a array/main.js
@@ -0,0 +1,5 @@
+"use strict";
+//Q20-Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let programming_languages = ["C++", "JavaScript", "TypeScript", "Python"];
+console.log("List of some programming languages");
+programming_languages.forEach(language => console.log(language));
---
+++ Q20-Think of something you could store in a array/main.ts
@@ -0,0 +1,7 @@
+//Q20-Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let programming_languages : string[] = ["C++", "JavaScript", "TypeScript", "Python"];
+
+console.log("List of some programming languages");
+
+programming_languages.forEach(language => console.log(language));
